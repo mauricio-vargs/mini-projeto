b4794927037e9147f9c1b8e7c7071f451b974308
Ivaav <mauricio.v27@outlook.com>
2024-03-25 21:14:30 -0300

    Função de seleção do pomodoro e função start

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+
+function selectTimer25(){
+    let timer25 = document.getElementById('25-5')
+    let timer50 = document.getElementById('50-10')
+
+    timer25.classList.add('selected')
+    timer50.classList.remove('selected')
+    
+}
+function selectTimer50(){
+    let timer25 = document.getElementById('25-5')
+    let timer50 = document.getElementById('50-10')
+
+    timer25.classList.remove('selected')
+    timer50.classList.add('selected')
+
+}
+
 function selectBeginner(){
     let beginner = document.getElementById('beginner')
     let intermediate = document.getElementById('intermediate')
@@ -27,4 +45,24 @@ function selectExpert(){
     intermediate.classList.remove('selected')
     expert.classList.add('selected')
     return      
+}
+
+function start(){
+    let configs = document.querySelectorAll('.selected')
+    
+    let pomodoroType = configs[0].innerText
+    let difficulty = configs[1].innerText
+
+    console.log(difficulty)
+    console.log(pomodoroType)
+    // getExercises(difficulty)
+    // getTimer(pomodoroType)
+}
+
+function getExercises(difficulty){
+
+}
+
+function getTimer(pomodoroType) {
+    
 }
