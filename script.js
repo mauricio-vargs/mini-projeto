46d3df089bc290e4516defe2fdb8a082edb8cb1a
Marcelo Vieira <marcelo_v2@estudante.sc.senai.br>
2024-03-31 00:25:03 -0300

    junção de códio da Darlene ao do Maurício 31_03

--- script.js
+++ script.js
@@ -1,119 +1,27 @@
 
-let exercisesList = []
-//contador para controlar passagem de exercícios
-let indice = 0;
-let offset = 0;
 
-//variaveis globais para estilização:
-let corpoPagina = document.querySelector('body');
-let divExercicio = document.createElement('div')
-let titulo = document.createElement('h2')
-let musculo = document.createElement('h3')
-let equipamento = document.createElement('h4')
-let paragrafo = document.createElement('p');
-let testinho = document.createElement('span')
-
-const apiKey = '???';
-function getExercises(difficulty) {
-    divExercicio.innerHTML = '';
-    fetch(`"https://api.api-ninjas.com/v1/exercises?type=stretching&difficulty=${difficulty}"` + offset, {
-
-        method: 'GET',
-        headers: { 'X-API-Key': '???'}, //TROCAR ANTES DE SUBIR!!!!!!!!
-        contentType: 'aplication/json'
-    }
-    )
-        .then((response) => response.json())
-        .then(dados => {
-            console.log(dados)
-            exercisesList = dados
-            //exibir exercício
-            showExercise()
-           
-            indice++
-        })
-        .catch(error => console.log(error))
-}
-
-//configurações da apresentação dos exercícios (inserida na function getExercises(), com FETCH)
-//há melhorias a serem feitas
-function showExercise() {
-
-    corpoPagina.appendChild(divExercicio)
-
-    titulo.textContent = `Exercício: ${exercisesList[indice].name}`
-    // Adicionando o parágrafo ao corpo da página
-    divExercicio.appendChild(titulo)
-
-    musculo.textContent = `Grupo muscular: ${exercisesList[indice].muscle}`
-    divExercicio.appendChild(musculo)
-
-    equipamento.textContent = `Equipamentos: ${exercisesList[indice].equipment}`
-    divExercicio.appendChild(equipamento)
-
-    paragrafo.textContent = `Instruções: ${exercisesList[indice].instructions}`//.name ou .muscle//JSON.stringify(exercisesList[indice]);mostra o conteúdo todo
-    divExercicio.appendChild(paragrafo);
-
-    testinho.textContent = (`Índice: ${indice}`);
-    console.log(testinho)
-    divExercicio.appendChild(testinho)
-
-    configStyle()
-
-   
-}
-
-//configs de estilo da div dos exercícios, está inserida na functio getExercises()
-function configStyle() {
-    divExercicio.style.margin = "auto";
-    divExercicio.style.width = "400px";
-    divExercicio.style.height = "400px";
-    divExercicio.style.display = "flex";
-    divExercicio.style.flexDirection = "column";
-    divExercicio.style.justifyContent = "right";
-    divExercicio.style.textAlign = "center;"
-    divExercicio.style.alignItems = "center";
-    divExercicio.style.backgroundColor = ""
-
-    paragrafo.style.cssText = 'background-color: gray; color: white; font-size: 20px; width: 400px; height: 300px; word-wrap: break-word; text-align: justify; padding: 8px;';
-    equipamento.style.color = "black";
-    equipamento.style.backgroundColor = "pink";
-
-    titulo.style.backgroundColor = "orange";
-    musculo.style.backgroundColor = "magenta";
-    testinho.style.backgroundColor = "orange";
-}
-
-function exerciseChange() {
-    
-
-    if (indice == 9) {
-        offset += 10
-        indice = 0
-        getExercises()
-    }
-    getExercises()
-    //indice++
-}
-
-function selectTimer25(){
+let time = ''
+let difficulty = ''
+let pomodoroType = ''
+let configs;
+let minutes;
+let seconds;
+function selectTimer25() {
     let timer25 = document.getElementById('25-5')
     let timer50 = document.getElementById('50-10')
 
     timer25.classList.add('selected')
     timer50.classList.remove('selected')
-    
 }
-function selectTimer50(){
+function selectTimer50() {
     let timer25 = document.getElementById('25-5')
     let timer50 = document.getElementById('50-10')
 
     timer25.classList.remove('selected')
     timer50.classList.add('selected')
-
 }
 
-function selectBeginner(){
+function selectBeginner() {
     let beginner = document.getElementById('beginner')
     let intermediate = document.getElementById('intermediate')
     let expert = document.getElementById('expert')
@@ -123,7 +31,8 @@ function selectBeginner(){
     expert.classList.remove('selected')
     return
 }
-function selectIntermediate(){
+
+function selectIntermediate() {
     let beginner = document.getElementById('beginner')
     let intermediate = document.getElementById('intermediate')
     let expert = document.getElementById('expert')
@@ -133,7 +42,8 @@ function selectIntermediate(){
     expert.classList.remove('selected')
     return
 }
-function selectExpert(){
+
+function selectExpert() {
     let beginner = document.getElementById('beginner')
     let intermediate = document.getElementById('intermediate')
     let expert = document.getElementById('expert')
@@ -141,57 +51,197 @@ function selectExpert(){
     beginner.classList.remove('selected')
     intermediate.classList.remove('selected')
     expert.classList.add('selected')
-    return      
+    return
 }
 
-function start(){
-    let configs = document.querySelectorAll('.selected')
-    
-    let pomodoroType = configs[0].innerText
-    let difficulty = configs[1].innerText
+//Aqui começa o módulo para iniciar e parar o timer
+
+let timeInterval;
+let startTime;
+let timeRunning = false;
+
+const startButton = document.getElementById('startButton');
+const stopButton = document.getElementById('stopButton');
+const timeStatus = document.getElementById('timeStatus');
+const timerDisplay = document.getElementById('timer');
+const changeExercicio = document.getElementById('changeExercicio');
+const mostraExercicio = document.getElementById('mostraExercicio')
+const classTime = document.querySelector('.time'); //para selecionar somente essas classe da main
+const classDifficulty = document.querySelector('.difficulty');
+
+
+startButton.addEventListener('click', start);
+
+stopButton.addEventListener('click', stop);
+
+changeExercicio.addEventListener('click', startExercicio)
+
+function start() {
+
+
+    configs = document.querySelectorAll('.selected')
+
+    pomodoroType = configs[0].innerText
+    difficulty = configs[1].innerText
 
     console.log(difficulty)
     console.log(pomodoroType)
-    // getExercises(difficulty)
-    // getTimer(pomodoroType)
+
+
+    timeRunning = true;
+    startButton.style.display = 'none';
+    stopButton.style.display = 'block';
+    startTime = new Date().getTime();
+    timeInterval = setInterval(updateTimer, 1000);
+
+    mostraExercicio.style.display = "none"; //apagar exercício ao reiniciar o timer
+
+
+
 }
 
-//funcao para surgirem botoes adicionados previamente no HTML--> adicionar ao "start pomodoro", após limpar a página inicial
-function showButtons() {
-    let btnObter = document.getElementById('btn-obter');
-    let btnTrocar = document.getElementById('btn-trocar');
-    btnObter.style.display = 'block';
-    btnTrocar.style.display = 'block';
+//Essa função faz com que apareça o timer seja decrescente em 
+//minuto e segundos. Pega também o tempo selecionado
+
+function updateTimer() {
+    const currentTime = new Date().getTime();
+    const elapsedTime = currentTime - startTime;
+    const elapsedMinutes = Math.floor(elapsedTime / (1000 * 60));
+
+    minutes = 0
+    seconds = 0
+
+    if (pomodoroType === '25-5') {
+        minutes = 24 - elapsedMinutes;
+        seconds = 60 - Math.floor((elapsedTime % (1000 * 60)) / 1000);
+        timerDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        timerStyle()
+        console.log(minutes)
+    } else if (pomodoroType === '50-10') {
+        minutes = 49 - elapsedMinutes;
+        seconds = 60 - Math.floor((elapsedTime % (1000 * 60)) / 1000);
+        timerDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+        timerStyle()
+    } else {
+        alert('Favor selecionar o tempo')
+        stop()
+    }
+
+    if (minutes < 0) {
+        //pegarExercicio() //<---------------------
+        timerExercicio()
+    }
 }
 
-//esta função contem os botões criados diretamente no JS, para surgirem quando trocar de página também 
-//mais configs serão acrescentadas
-function dinamicButton(){
-    let divDinamica = document.createElement("div")
-    document.body.appendChild(divDinamica);
-    
-    let btnStretching = document.createElement("button");
-    btnStretching.innerHTML = "OBTER ALONGAMENTO";
-    btnStretching.onclick = getExercises;
-    divDinamica.appendChild(btnStretching)
-    
-    let btnSkip = document.createElement("button");
-    btnSkip.innerHTML = "PROXIMO";
-    btnSkip.onclick = exerciseChange;
-    divDinamica.appendChild(btnSkip)
-    
-    divDinamica.style.backgroundColor = "red";
-    divDinamica.style.display = "flex";
-    divDinamica.style.flexDirection = "column";
-    divDinamica.style.width = "180px";
-    
-    divDinamica.style.position = "absolute";
-    divDinamica.style.bottom = "300px";
-    divDinamica.style.right = "300px";
+function timerExercicio() {
+    changeExercicio.style.display = 'none';
+    const currentTime = new Date().getTime();
+    const elapsedTime = currentTime - startTime;
+    const elapsedMinutes = Math.floor(elapsedTime / (1000 * 60));
+    console.log(pomodoroType)
+    if (pomodoroType === '25-5') {
+        minutes = 0 - elapsedMinutes;
+        seconds = 60 - Math.floor((elapsedTime % (1000 * 60)) / 1000);
+    } else {
+        minutes = 10 - elapsedMinutes;
+        seconds = 60 - Math.floor((elapsedTime % (1000 * 60)) / 1000);
+    }
+    timerDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    if (minutes < 0) {
+        start()
+    }
 }
-//é necessário encaixar no local certo para ser chamada
-dinamicButton()
 
-function getTimer(pomodoroType) {
-    
+function timerStyle() {
+    timerDisplay.style.fontSize = "80px";
+    timerDisplay.style.color = "white";
+    timerDisplay.style.backgroundImage = "url('./imagem-pomodoro.jpg')";
+    timerDisplay.style.borderRadius = "8px";
+    timerDisplay.style.backgroundSize = "cover";
+    timerDisplay.style.backgroundPosition = "center";
+    timerDisplay.style.width = "50%";
+    timerDisplay.style.height = "30%";
+    timerDisplay.style.display = "flex";
+    timerDisplay.style.justifyContent = "center";
+    timerDisplay.style.alignItems = "center";
 }
+
+function stop() {
+    if (timeRunning) {
+        clearInterval(timeInterval);
+        timeRunning = false;
+        startButton.style.display = 'block';
+        stopButton.style.display = 'none';
+        changeExercicio.style.display = 'block'; //<---------------obter/mudar exercício somente no pause
+        time = '';
+        minutes = 0;
+        seconds = 0;
+        timerDisplay.style.backgroundImage = '';
+        timerDisplay.textContent = '';
+
+    }
+}
+
+//Aqui começa o módulo de exercícios
+
+let listaExercicios = []
+let exercicioAtual = 0
+let offset = 0
+
+//<--------------conferir o posicionamento das funções
+const apiKey = 'ZB9COpRrl8yxkDb9slnytA==bgJmdTLUIyUXNKXO'
+function pegarExercicio() {
+    fetch("https://api.api-ninjas.com/v1/exercises?type=stretching&difficulty=" + difficulty + "&offset=" + offset, {
+        method: 'GET',
+        headers: { 'X-Api-Key': apiKey },
+        contentType: 'application/json',
+    })
+        .then(response => response.json())
+        .then(dados => {
+            listaExercicios = dados
+            exibirExercicio() // <----------------------
+        })
+
+        .catch(error => console.log(error))
+}
+
+function exercicioStyle() {
+    mostraExercicio.style.backgroundColor = "white"
+    mostraExercicio.style.borderRadius = "8px";
+    mostraExercicio.style.width = "50%";
+    mostraExercicio.style.height = "30%";
+    mostraExercicio.style.display = "flex";
+    mostraExercicio.style.justifyContent = "center";
+    mostraExercicio.style.alignItems = "center";
+    mostraExercicio.style.flexDirection = "column";
+    mostraExercicio.style.padding = "20px"
+}
+
+function startExercicio() {
+    classTime.style.display = 'none';//<-- para esconder os botões (exceto iniciar) e haver espaço pros exercícios
+    classDifficulty.style.display = 'none';
+    if (exercicioAtual === 9) {
+        offset = offset + 10
+        exercicioAtual = 0
+        pegarExercicio()
+        return
+    }
+    pegarExercicio()
+    exercicioAtual++
+}
+
+function exibirExercicio() {
+    const nomeExercicio = document.getElementById("nomeExercicio")
+    const grupoMuscular = document.getElementById("grupoMuscular")
+    const descricaoExercicio = document.getElementById("descricaoExercicio")
+    const equipamento = document.getElementById('equipamento')
+
+    nomeExercicio.textContent = `Exercício: ${listaExercicios[exercicioAtual].name}`
+    grupoMuscular.textContent = `Grupo Muscular: ${listaExercicios[exercicioAtual].muscle}`
+    equipamento.textContent = `Equipamento: ${listaExercicios[exercicioAtual].equipment}`
+    descricaoExercicio.textContent = `Instruções: ${listaExercicios[exercicioAtual].instructions}`
+    exercicioStyle()
+}
+
+
+
